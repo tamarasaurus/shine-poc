b49589e3666701024aa73528b8e6e7698c2faf36
Tamara Robichet <tamara.robichet@akeneo.com>
2019-05-16 15:20:12 +0200

    add hook api

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express()
+
+app.options('*', cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  next()
+})
+
+app.post('/hook', cors(), (req, res) => {
+  console.log('body', req.body);
+  res.status(200);
+});
+
+app.listen(process.env.PORT || '8080', () => console.log(`Example app listening on port 8000`))
