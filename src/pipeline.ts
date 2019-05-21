5390bd64250fad090740839752a211753d1f6bd5
Tamara Robichet <tamara.robichet@akeneo.com>
2019-05-21 18:26:18 +0200

    Use job queue

--- src/pipeline.ts
+++ src/pipeline.ts
@@ -1,33 +1,65 @@
 import { execSync } from 'child_process';
 import { resolve } from 'path';
+import * as Bull from 'bull'
+import * as config from './inspectors/config.json'
+
+interface InspectorConfig {
+  [name: string]: {
+    setup?: string;
+    inspect: string;
+    teardown?: string;
+  }
+}
+
+interface EventData {
+  user: string;
+  commit: string;
+}
 
 export default class Pipeline {
-  private eventData;
+  private eventData: EventData;
 
-  constructor(eventData: { user: string, commit: string }) {
+  constructor(eventData: EventData) {
     this.eventData = eventData;
-    console.log('Launch pipeline', eventData);
+  }
+
+  async runInspection(job: Bull.Job, done: Bull.DoneCallback) {
+    const inspectorConfig: InspectorConfig = config;
+    const inspector = inspectorConfig[job.data.name]
+    const { setup, inspect, teardown } = inspector;
+
+    try {
+      if (setup) {
+        await execSync(setup)
+      }
+
+      const data = await execSync(inspect);
+
+      console.log(data.toString())
+      if (teardown) {
+        await execSync(teardown)
+      }
+
+      done(null, JSON.parse(data.toString()));
+    } catch (e) {
+      done(e);
+    }
   }
 
   launch() {
-    // Use a static config object instead of bash scripts
-    const inspectors = {
-      scrutinizer: 'scrutinizer.sh',
-      phpstan: 'phpstan.sh',
-      phpmetrics: 'phpmetrics.sh',
-    }
+    const queue = new Bull('inspect',  'redis://localhost:6379');
 
-    const analysis = {};
-    execSync(resolve(process.cwd(), './src/setup.sh'), { stdio: 'inherit' });
+    queue.clean(10).then(() => {
+      queue.process('inspect', 1, this.runInspection)
 
-    for (let inspector in inspectors) {
-      const script = inspectors[inspector];
-      console.log('Start', inspector)
-      const data = execSync(resolve(process.cwd(), `./src/inspectors/${script}`))
-      analysis[inspector] = JSON.parse(data.toString());
-    }
-
-    console.log(analysis);
-    execSync(resolve(process.cwd(), './src/teardown.sh'));
+      const [name, options] = Object.entries(config)[0]
+      // .forEach(([name, options]) => {
+        console.log(name, options)
+        queue.add('inspect', Object.assign({
+          name,
+          inspectorName: name
+        }, options));
+      // })
+    })
   }
 }
--- src/setup.sh
+++ src/setup.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-export DOCKER_PORT_HTTP=8080
-export DOCKER_PORT_HTTP_BEHAT=8081
-
+# checkout the same commit as the github hook
 git clone git@github.com:akeneo/pim-community-dev.git tmp/pim-community-dev
 cd tmp/pim-community-dev
-# checkout the same commit as the github hook
+cp .env.dist .env
+
 docker-compose up -d fpm
 docker-compose exec -T fpm php -d memory_limit=3G /usr/local/bin/composer install
