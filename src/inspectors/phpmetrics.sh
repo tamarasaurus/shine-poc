790cf4ac2b65cf6b42994eed78ef20ed83de9b02
Tamara Robichet <tamara@robichet.com>
2019-05-21 22:45:09 +0200

    Wait for phpmetrics

--- src/inspectors/phpmetrics.sh
+++ src/inspectors/phpmetrics.sh
@@ -3,4 +3,5 @@
 cd tmp/pim-community-dev
 docker-compose exec -T fpm php -d memory_limit=3G /usr/local/bin/composer --quiet require phpmetrics/phpmetrics
 docker-compose exec -T fpm php ./vendor/bin/phpmetrics --quiet src/Akeneo/Pim --report-json=phpmetrics.json
+sleep 2
 docker-compose exec -T fpm cat phpmetrics.json
