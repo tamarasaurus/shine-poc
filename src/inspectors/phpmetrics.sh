830bfebe86a186cac605d51a21fec9a58d7a985d
Tamara Robichet <tamara.robichet@akeneo.com>
2019-05-22 17:19:41 +0200

    wip

--- src/inspectors/phpmetrics.sh
+++ src/inspectors/phpmetrics.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-cd pim-community-dev
-php -d memory_limit=3G /usr/local/bin/composer --quiet require phpmetrics/phpmetrics
-php ./vendor/bin/phpmetrics --quiet --report-json=phpmetrics.json src/Akeneo/Pim
+php -d memory_limit=3G composer --quiet require phpmetrics/phpmetrics
+php ./vendor/bin/phpmetrics --quiet --report-json=phpmetrics.json pim-community-dev/src/Akeneo/Pim
 sleep 15
 cat phpmetrics.json
