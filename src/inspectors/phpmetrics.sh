df440ca38ee461798c8a3baec08cc013f60737f7
Tamara Robichet <tamara.robichet@akeneo.com>
2019-05-23 16:20:25 +0200

    fix paths

--- src/inspectors/phpmetrics.sh
+++ src/inspectors/phpmetrics.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
+cd pim-community-dev
 php ./vendor/bin/phpmetrics --report-json=phpmetrics.json pim-community-dev/src/Akeneo/Pim
 sleep 15
 cat phpmetrics.json | jq --arg commit "$COMMIT" --arg name "phpmetrics" '{ commit: $commit, name: $name, inspection: .}' | curl -H "Content-Type: application/json" -X POST -d @- https://shine-poc.herokuapp.com/job
-
-
+cd ../
--- src/inspectors/phpstan.sh
+++ src/inspectors/phpstan.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-php vendor/bin/phpstan analyse -c pim-community-dev/phpstan.neon pim-community-dev/src/Akeneo/Pim -l 1 --error-format=json --no-progress --no-interaction > phpstan.json
+cd pim-community-dev && php vendor/bin/phpstan analyse -c phpstan.neon src/Akeneo/Pim -l 1 --error-format=json --no-progress --no-interaction > phpstan.json
 cat phpstan.json | jq --arg commit "$COMMIT" --arg name "phpstan" '{ commit: $commit, name: $name, inspection: .}' | curl -H "Content-Type: application/json" -X POST -d @- https://shine-poc.herokuapp.com/job
