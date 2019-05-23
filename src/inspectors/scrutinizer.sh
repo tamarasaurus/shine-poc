ebd6649e0e47d9714cbc1437a4be8beb4af8dfb7
Tamara Robichet <tamara.robichet@akeneo.com>
2019-05-23 13:43:32 +0200

    wip

--- src/inspectors/scrutinizer.sh
+++ src/inspectors/scrutinizer.sh
@@ -8,3 +8,5 @@
 # curl -s GET https://scrutinizer-ci.com/api/repositories/g/akeneo/pim-community-dev/inspections?sort_field=finished_at
 
 curl -s GET "https://scrutinizer-ci.com/api/repositories/g/akeneo/pim-community-dev/inspections/8aa1c7d3-56fe-4c88-8df5-938bd8504b9f" | jq '._embedded.index_diff * ._embedded.repository.applications.master.index._embedded.project.metric_values' | jq --arg commit "$COMMIT" --arg name "scrutinizer" '{ commit: $commit, name: $name, inspection: .}' | curl -H "Content-Type: application/json" -X POST -d @- https://shine-poc.herokuapp.com/job
+
+# set up webhook for finishing
--- src/inspectors/sonarcube.sh
+++ src/inspectors/sonarcube.sh
@@ -1,1 +1,5 @@
 echo '{}'
+
+# trigger analysis
+# set up webhook on sonarcloud when analysis is done
+
