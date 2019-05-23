bfd64c6a4435d95683cf900461a72efdb51d2e1a
Tamara Robichet <tamara.robichet@akeneo.com>
2019-05-23 16:26:51 +0200

    fix paths

--- src/inspectors/phpmetrics.sh
+++ src/inspectors/phpmetrics.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-cd pim-community-dev
-php ./vendor/bin/phpmetrics --report-json=phpmetrics.json pim-community-dev/src/Akeneo/Pim
+cd pim-community-dev && php ./vendor/bin/phpmetrics --report-json=phpmetrics.json src/Akeneo/Pim
 sleep 15
 cat phpmetrics.json | jq --arg commit "$COMMIT" --arg name "phpmetrics" '{ commit: $commit, name: $name, inspection: .}' | curl -H "Content-Type: application/json" -X POST -d @- https://shine-poc.herokuapp.com/job
 cd ../
