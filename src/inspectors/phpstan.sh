ccdfbb04ca004902eeb0221de01cebfceb048e7a
Tamara Robichet <tamara.robichet@akeneo.com>
2019-05-23 09:34:20 +0200

    wip

--- src/inspectors/phpstan.sh
+++ src/inspectors/phpstan.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-php vendor/bin/phpstan analyse -c phpstan.neon pim-community-dev/src/Akeneo/Pim -l 1 --error-format=json --no-progress --no-interaction > phpstan.json
+php vendor/bin/phpstan analyse -c pim-community-dev/phpstan.neon pim-community-dev/src/Akeneo/Pim -l 1 --error-format=json --no-progress --no-interaction > phpstan.json
 cat phpstan.json | curl -H "Content-Type: application/json" -X POST -d @- https://shine-poc.herokuapp.com/job
